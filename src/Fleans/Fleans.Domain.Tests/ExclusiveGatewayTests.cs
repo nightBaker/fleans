85a8c0acb9652a13ced12b6b740effd0205eb120
Nurzhan Aitbayev <nurzhan@outlook.com>
2023-11-02 05:30:13 +0600

    remove moq

--- src/Fleans/Fleans.Domain.Tests/ExclusiveGatewayTests.cs
+++ src/Fleans/Fleans.Domain.Tests/ExclusiveGatewayTests.cs
@@ -1,4 +1,3 @@
-using Moq;
 using NSubstitute;
 
 namespace Fleans.Domain.Tests
@@ -6,48 +5,6 @@ namespace Fleans.Domain.Tests
     [TestClass]
     public class ExclusiveGatewayTests
     {
-        [TestMethod]
-        public async Task IfStatement_ShouldRun_TrueBranch_moq()
-        {
-            // Arrange
-            var workflowVersion = 1;
-
-            var conditionRunner = new Mock<IConditionExpressionRunner>();
-            conditionRunner.Setup(x => x.Evaluate(It.IsAny<IContext>())).Returns(true);
-
-            var conditionBuilder = new Mock<IConditionBuilder>();
-            conditionBuilder.Setup(x => x.Build()).Returns(conditionRunner.Object);
-
-            var activity = new Mock<IActivity>();
-            activity.Setup(x => x.ExecuteAsync(It.IsAny<IContext>())).Returns(Task.CompletedTask).Verifiable();
-
-            var thenActivityBuilder = new Mock<IActivityBuilder>();
-            thenActivityBuilder.Setup(x => x.WithId(It.IsAny<Guid>())).Returns(thenActivityBuilder.Object);
-            thenActivityBuilder.Setup(x => x.Build()).Returns(activity.Object);
-
-            var elseActivityBuilder = new Mock<IActivityBuilder>();
-            elseActivityBuilder.Setup(x => x.WithId(It.IsAny<Guid>())).Returns(elseActivityBuilder.Object);
-
-            var workflow = new WorkflowBuilder()
-                .StartWith(new Dictionary<string, object>
-                            {
-                                ["expressionVariable"] = true
-                            },
-                        new ExclusiveGatewayBuilder()
-                            .Condition(conditionBuilder.Object)
-                            .Then(thenActivityBuilder.Object, Guid.NewGuid())
-                            .Else(elseActivityBuilder.Object, Guid.NewGuid())
-                            .WithId(Guid.NewGuid()))
-                .Build(Guid.NewGuid(), workflowVersion);
-
-            // Act
-            await workflow.Run();
-
-
-            // Assert
-            activity.Verify(x => x.ExecuteAsync(It.IsAny<IContext>()), Times.Once);
-        }
-
         [TestMethod]
         public async Task IfStatement_ShouldRun_TrueBranch()
         {
