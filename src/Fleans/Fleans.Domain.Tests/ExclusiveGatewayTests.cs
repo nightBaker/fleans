e08a220c6a481e127d918710225f47f7b821abde
Nurzhan Aitbayev <n-ait@mail.ru>
2023-11-05 19:56:45 +0600

    add NSubstitute to test (#2)
    * add NSubstitute to test
    
    * remove moq
    
    ---------
    
    Co-authored-by: Nurzhan Aitbayev <nurzhan@outlook.com>

--- src/Fleans/Fleans.Domain.Tests/ExclusiveGatewayTests.cs
+++ src/Fleans/Fleans.Domain.Tests/ExclusiveGatewayTests.cs
@@ -1,4 +1,4 @@
-using Moq;
+using NSubstitute;
 
 namespace Fleans.Domain.Tests
 {
@@ -8,37 +8,88 @@ namespace Fleans.Domain.Tests
         [TestMethod]
         public async Task IfStatement_ShouldRun_TrueBranch()
         {
-            var conditionRunner = new Mock<IConditionExpressionRunner>();
-            conditionRunner.Setup(x => x.Evaluate(It.IsAny<IContext>())).Returns(true);
+            // Arrange
+            var workflowVersion = 1;
 
-            var conditionBuilder = new Mock<IConditionBuilder>();
-            conditionBuilder.Setup(x => x.Build()).Returns(conditionRunner.Object);
+            var conditionRunner = Substitute.For<IConditionExpressionRunner>();
+            conditionRunner.Evaluate(Arg.Any<IContext>()).Returns(true);
 
-            var activity = new Mock<IActivity>();
-            activity.Setup(x => x.ExecuteAsync(It.IsAny<IContext>())).Returns(Task.CompletedTask).Verifiable();
+            var conditionBuilder = Substitute.For<IConditionBuilder>();
+            conditionBuilder.Build().Returns(conditionRunner);
 
-            var activityBuilder = new Mock<IActivityBuilder>();
-            activityBuilder.Setup(x => x.WithId(It.IsAny<Guid>())).Returns(activityBuilder.Object);
-            activityBuilder.Setup(x => x.Build()).Returns(activity.Object);
+            var activity = Substitute.For<IActivity>();
+            activity.ExecuteAsync(Arg.Any<IContext>()).Returns(Task.CompletedTask);
 
-            var elseActivityBuilder = new Mock<IActivityBuilder>();
-            elseActivityBuilder.Setup(x => x.WithId(It.IsAny<Guid>())).Returns(elseActivityBuilder.Object);
+            var thenActivityBuilder = Substitute.For<IActivityBuilder>();
+            thenActivityBuilder.WithId(Arg.Any<Guid>()).Returns(thenActivityBuilder);
+            thenActivityBuilder.Build().Returns(activity);
 
-            var workflow = new WorkflowBuilder()
+            var elseActivityBuilder = Substitute.For<IActivityBuilder>();
+            elseActivityBuilder.WithId(Arg.Any<Guid>()).Returns(elseActivityBuilder);
+
+            var workflowBuilder = new WorkflowBuilder();
+            var workflow = workflowBuilder
                 .StartWith(new Dictionary<string, object>
-                            {
-                                ["expressionVariable"] = true
-                            },
-                        new ExclusiveGatewayBuilder()
-                            .Condition(conditionBuilder.Object)
-                            .Then(activityBuilder.Object, Guid.NewGuid())
-                            .Else(elseActivityBuilder.Object, Guid.NewGuid())
-                            .WithId(Guid.NewGuid()))
-                .Build(Guid.NewGuid(), 1);
+                {
+                    ["expressionVariable"] = true
+                }, new ExclusiveGatewayBuilder()
+                    .Condition(conditionBuilder)
+                    .Then(thenActivityBuilder, Guid.NewGuid())
+                    .Else(elseActivityBuilder, Guid.NewGuid())
+                    .WithId(Guid.NewGuid()))
+                .Build(Guid.NewGuid(), workflowVersion);
 
+            // Act
             await workflow.Run();
 
-            activity.Verify(x => x.ExecuteAsync(It.IsAny<IContext>()), Times.Once);
+            // Assert
+            _ = activity.Received(1).ExecuteAsync(Arg.Any<IContext>());
+        }
+
+        [TestMethod]
+        public async Task IfStatement_ShouldNotRun_FalseBranch()
+        {
+            // Arrange
+            var workflowVersion = 1;
+
+            var conditionRunner = Substitute.For<IConditionExpressionRunner>();
+            conditionRunner.Evaluate(Arg.Any<IContext>()).Returns(true);
+
+            var conditionBuilder = Substitute.For<IConditionBuilder>();
+            conditionBuilder.Build().Returns(conditionRunner);
+
+            var thenActivity = Substitute.For<IActivity>();
+            thenActivity.ExecuteAsync(Arg.Any<IContext>()).Returns(Task.CompletedTask);
+
+            var thenActivityBuilder = Substitute.For<IActivityBuilder>();
+            thenActivityBuilder.WithId(Arg.Any<Guid>()).Returns(thenActivityBuilder);
+            thenActivityBuilder.Build().Returns(thenActivity);
+
+
+            var elseActivity = Substitute.For<IActivity>();
+            elseActivity.ExecuteAsync(Arg.Any<IContext>()).Returns(Task.CompletedTask);
+
+            var elseActivityBuilder = Substitute.For<IActivityBuilder>();
+            elseActivityBuilder.WithId(Arg.Any<Guid>()).Returns(elseActivityBuilder);
+            elseActivityBuilder.Build().Returns(elseActivity);
+
+            var workflowBuilder = new WorkflowBuilder();
+            var workflow = workflowBuilder
+                .StartWith(new Dictionary<string, object>
+                {
+                    ["expressionVariable"] = true
+                }, new ExclusiveGatewayBuilder()
+                    .Condition(conditionBuilder)
+                    .Then(thenActivityBuilder, Guid.NewGuid())
+                    .Else(elseActivityBuilder, Guid.NewGuid())
+                    .WithId(Guid.NewGuid()))
+                .Build(Guid.NewGuid(), workflowVersion);
+
+            // Act
+            await workflow.Run();
+
+            // Assert
+            _ = elseActivity.Received(0).ExecuteAsync(Arg.Any<IContext>());
         }
     }
 }
