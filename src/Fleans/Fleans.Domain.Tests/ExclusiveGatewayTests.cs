e88c6a58caf5c3551999586d3df01a1263a08fb7
Yerassyl <y.shalabayev@sweedpos.com>
2023-11-22 23:05:26 +0600

    tests simplification

--- src/Fleans/Fleans.Domain.Tests/ExclusiveGatewayTests.cs
+++ src/Fleans/Fleans.Domain.Tests/ExclusiveGatewayTests.cs
@@ -1,3 +1,4 @@
+using Fleans.Domain.Connections;
 using NSubstitute;
 using System.Diagnostics;
 
@@ -6,120 +7,78 @@ namespace Fleans.Domain.Tests
     [TestClass]
     public class ExclusiveGatewayTests
     {
+        
         [TestMethod]
         public async Task IfStatement_ShouldRun_ThenBranchNotElse()
         {
-            // Arrange
-            var workflowVersion = 1;
-
+            // Arrange           
             var conditionRunner = Substitute.For<IConditionExpressionRunner>();
             conditionRunner.Evaluate(Arg.Any<IContext>()).Returns(true);
+            
+            var exclusiveGateway = new ExclusiveGatewayActivity(Guid.NewGuid(), conditionRunner);
 
-            var conditionBuilder = Substitute.For<IConditionBuilder>();
-            conditionBuilder.Build().Returns(conditionRunner);
-
-            var activity = Substitute.For<IActivity>();
-            activity.Id.Returns(Guid.NewGuid());
-            activity.ExecuteAsync(Arg.Any<IContext>()).Returns(new ActivityExecutionResult(ActivityResultStatus.Completed));
-            activity.Status.Returns(ActivityStatus.Completed);
-
-            var thenActivityBuilder = Substitute.For<IActivityBuilder>();
-            thenActivityBuilder.Build().Returns(new ActivityBuilderResult
-            {
-                Activity = activity,
-                ChildActivities = new List<IActivity>(),
-                Connections = new List<IWorkflowConnection<IActivity, IActivity>>()
-            });
-
+            var thenActivity = Substitute.For<IActivity>();
+            thenActivity.Id.Returns(Guid.NewGuid());
+            thenActivity.ExecuteAsync(Arg.Any<IContext>()).Returns(new ActivityExecutionResult(ActivityResultStatus.Completed));
+            thenActivity.Status.Returns(ActivityStatus.Completed);            
+            
             var elseActivity = Substitute.For<IActivity>();
             elseActivity.Id.Returns(Guid.NewGuid());
             elseActivity.ExecuteAsync(Arg.Any<IContext>()).Returns(new ActivityExecutionResult(ActivityResultStatus.Completed));
-
-            var elseActivityBuilder = Substitute.For<IActivityBuilder>();
-            elseActivityBuilder.Build().Returns(new ActivityBuilderResult
-            {
-                Activity = elseActivity,
-                ChildActivities = new List<IActivity>(),
-                Connections = new List<IWorkflowConnection<IActivity, IActivity>>()
-            });
-
-            var workflowBuilder = new WorkflowBuilder();
-            var workflow = workflowBuilder
-                .With(new Dictionary<string, object>
+           
+            var workflowDefinition = new WorkflowDefinition(Guid.NewGuid(), 1,
+                new[] { exclusiveGateway, thenActivity , elseActivity },
+                new Dictionary<Guid, IWorkflowConnection<IActivity, IActivity>[]>
                 {
-                    ["expressionVariable"] = true
-                })
-                .With(new WorkflowDefinitionBuilder(Guid.NewGuid(), workflowVersion)
-                    .StartWith(new ExclusiveGatewayBuilder(Guid.NewGuid())
-                            .Condition(conditionBuilder)
-                            .Then(thenActivityBuilder)
-                            .Else(elseActivityBuilder)))
-                .Build(Guid.NewGuid());
+                    [exclusiveGateway.Id] = new[] { 
+                        new ExclusiveGatewayConnection(exclusiveGateway, thenActivity, true),
+                        new ExclusiveGatewayConnection(exclusiveGateway, elseActivity, false) 
+                    },                    
+                });
+            var workflow = new Workflow(Guid.NewGuid(), new(), exclusiveGateway, workflowDefinition);
 
             // Act
             await workflow.Run();
 
             // Assert
-            _ = activity.Received(1).ExecuteAsync(Arg.Any<IContext>());
+            _ = thenActivity.Received(1).ExecuteAsync(Arg.Any<IContext>());
             _ = elseActivity.Received(0).ExecuteAsync(Arg.Any<IContext>());
         }
 
         [TestMethod]
         public async Task IfStatement_ShouldRun_ElseBranchNotThen()
         {
-            // Arrange
-            var workflowVersion = 1;
-
+            // Arrange           
             var conditionRunner = Substitute.For<IConditionExpressionRunner>();
-            conditionRunner.Evaluate(Arg.Any<IContext>()).Returns(false);            
+            conditionRunner.Evaluate(Arg.Any<IContext>()).Returns(false);
 
-            var conditionBuilder = Substitute.For<IConditionBuilder>();
-            conditionBuilder.Build().Returns(conditionRunner);
+            var exclusiveGateway = new ExclusiveGatewayActivity(Guid.NewGuid(), conditionRunner);
 
-            var activity = Substitute.For<IActivity>();
-            activity.Id.Returns(Guid.NewGuid());
-            activity.ExecuteAsync(Arg.Any<IContext>()).Returns(new ActivityExecutionResult(ActivityResultStatus.Completed));
-
-
-            var thenActivityBuilder = Substitute.For<IActivityBuilder>();
-            thenActivityBuilder.Build().Returns(new ActivityBuilderResult
-            {
-                Activity = activity,
-                ChildActivities = new List<IActivity>(),
-                Connections = new List<IWorkflowConnection<IActivity, IActivity>>()
-            });
+            var thenActivity = Substitute.For<IActivity>();
+            thenActivity.Id.Returns(Guid.NewGuid());
+            thenActivity.ExecuteAsync(Arg.Any<IContext>()).Returns(new ActivityExecutionResult(ActivityResultStatus.Completed));            
 
             var elseActivity = Substitute.For<IActivity>();
             elseActivity.Id.Returns(Guid.NewGuid());
             elseActivity.ExecuteAsync(Arg.Any<IContext>()).Returns(new ActivityExecutionResult(ActivityResultStatus.Completed));
             elseActivity.Status.Returns(ActivityStatus.Completed);
 
-            var elseActivityBuilder = Substitute.For<IActivityBuilder>();
-            elseActivityBuilder.Build().Returns(new ActivityBuilderResult
-            {
-                Activity = elseActivity,
-                ChildActivities = new List<IActivity>(),
-                Connections = new List<IWorkflowConnection<IActivity, IActivity>>()
-            });
-
-            var workflowBuilder = new WorkflowBuilder();
-            var workflow = workflowBuilder
-                .With(new Dictionary<string, object>
+            var workflowDefinition = new WorkflowDefinition(Guid.NewGuid(), 1,
+                new[] { exclusiveGateway, thenActivity, elseActivity },
+                new Dictionary<Guid, IWorkflowConnection<IActivity, IActivity>[]>
                 {
-                    ["expressionVariable"] = true
-                })
-                .With(new WorkflowDefinitionBuilder(Guid.NewGuid(), workflowVersion)
-                    .StartWith(new ExclusiveGatewayBuilder(Guid.NewGuid())
-                            .Condition(conditionBuilder)
-                            .Then(thenActivityBuilder)
-                            .Else(elseActivityBuilder)))
-                .Build(Guid.NewGuid());
+                    [exclusiveGateway.Id] = new[] {
+                        new ExclusiveGatewayConnection(exclusiveGateway, thenActivity, true),
+                        new ExclusiveGatewayConnection(exclusiveGateway, elseActivity, false)
+                    },
+                });
+            var workflow = new Workflow(Guid.NewGuid(), new(), exclusiveGateway, workflowDefinition);
 
             // Act
             await workflow.Run();
 
             // Assert
-            _ = activity.Received(0).ExecuteAsync(Arg.Any<IContext>());
+            _ = thenActivity.Received(0).ExecuteAsync(Arg.Any<IContext>());
             _ = elseActivity.Received(1).ExecuteAsync(Arg.Any<IContext>());
         }
 
